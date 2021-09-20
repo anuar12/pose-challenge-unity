04137c9f1e5af438f9218be5e2927efc1f1acf8a
Anuar Yeraliyev <anuar.yeraliyev@gmail.com>
2021-09-20 15:21:49 -0400

    convert indents to spaces

--- Assets/Scripts/Runtime/DrawSkeleton.cs
+++ Assets/Scripts/Runtime/DrawSkeleton.cs
@@ -11,9 +11,9 @@ public class DrawSkeleton : MonoBehaviour
 	private int[][] jointPairs;
 	private float lineWidth = 5.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
 		int numPairs = keypoints.Length + 1;
 		lines = new GameObject[numPairs];
 		lineRenderers = new LineRenderer[numPairs];
@@ -21,96 +21,103 @@ public class DrawSkeleton : MonoBehaviour
 
 		InitializeSkeleton();
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
-    }
+	}
 
-	void LateUpdate() {
+	void LateUpdate()
+	{
 		RenderSkeleton();
 	}
 
 
-	private void InitializeLine(int pairIndex, int startIndex, int endIndex, float width, Color color) {
-        jointPairs[pairIndex] = new int[] { startIndex, endIndex };
-        string name = $"{keypoints[startIndex].name}_to_{keypoints[endIndex].name}";
-        lines[pairIndex] = new GameObject(name);
+	private void InitializeLine(int pairIndex, int startIndex, int endIndex, float width, Color color)
+	{
+		jointPairs[pairIndex] = new int[] { startIndex, endIndex };
+		string name = $"{keypoints[startIndex].name}_to_{keypoints[endIndex].name}";
+		lines[pairIndex] = new GameObject(name);
 
-        lineRenderers[pairIndex] = lines[pairIndex].AddComponent<LineRenderer>();
-        lineRenderers[pairIndex].material = new Material(Shader.Find("Unlit/Color"));
-        lineRenderers[pairIndex].material.color = color;
+		lineRenderers[pairIndex] = lines[pairIndex].AddComponent<LineRenderer>();
+		lineRenderers[pairIndex].material = new Material(Shader.Find("Unlit/Color"));
+		lineRenderers[pairIndex].material.color = color;
 
-        lineRenderers[pairIndex].positionCount = 2;
-        lineRenderers[pairIndex].startWidth = width;
-        lineRenderers[pairIndex].endWidth = width;
-    }
+		lineRenderers[pairIndex].positionCount = 2;
+		lineRenderers[pairIndex].startWidth = width;
+		lineRenderers[pairIndex].endWidth = width;
+	}
 
-	private void InitializeSkeleton() {
-        // Nose to left eye
-        InitializeLine(0, 0, 1, lineWidth, Color.magenta);
-        // Nose to right eye
-        InitializeLine(1, 0, 2, lineWidth, Color.magenta);
-        // Left eye to left ear
-        InitializeLine(2, 1, 3, lineWidth, Color.magenta);
-        // Right eye to right ear
-        InitializeLine(3, 2, 4, lineWidth, Color.magenta);
+	private void InitializeSkeleton()
+	{
+		// Nose to left eye
+		InitializeLine(0, 0, 1, lineWidth, Color.magenta);
+		// Nose to right eye
+		InitializeLine(1, 0, 2, lineWidth, Color.magenta);
+		// Left eye to left ear
+		InitializeLine(2, 1, 3, lineWidth, Color.magenta);
+		// Right eye to right ear
+		InitializeLine(3, 2, 4, lineWidth, Color.magenta);
 
-        // Left shoulder to right shoulder
-        InitializeLine(4, 5, 6, lineWidth, Color.red);
-        // Left shoulder to left hip
-        InitializeLine(5, 5, 11, lineWidth, Color.red);
-        // Right shoulder to right hip
-        InitializeLine(6, 6, 12, lineWidth, Color.red);
-        // Left shoulder to right hip
-        InitializeLine(7, 5, 12, lineWidth, Color.red);
-        // Right shoulder to left hip
-        InitializeLine(8, 6, 11, lineWidth, Color.red);
-        // Left hip to right hip
-        InitializeLine(9, 11, 12, lineWidth, Color.red);
+		// Left shoulder to right shoulder
+		InitializeLine(4, 5, 6, lineWidth, Color.red);
+		// Left shoulder to left hip
+		InitializeLine(5, 5, 11, lineWidth, Color.red);
+		// Right shoulder to right hip
+		InitializeLine(6, 6, 12, lineWidth, Color.red);
+		// Left shoulder to right hip
+		InitializeLine(7, 5, 12, lineWidth, Color.red);
+		// Right shoulder to left hip
+		InitializeLine(8, 6, 11, lineWidth, Color.red);
+		// Left hip to right hip
+		InitializeLine(9, 11, 12, lineWidth, Color.red);
 
-        // Left Arm
-        InitializeLine(10, 5, 7, lineWidth, Color.green);
-        InitializeLine(11, 7, 9, lineWidth, Color.green);
-        // Right Arm
-        InitializeLine(12, 6, 8, lineWidth, Color.green);
-        InitializeLine(13, 8, 10, lineWidth, Color.green);
+		// Left Arm
+		InitializeLine(10, 5, 7, lineWidth, Color.green);
+		InitializeLine(11, 7, 9, lineWidth, Color.green);
+		// Right Arm
+		InitializeLine(12, 6, 8, lineWidth, Color.green);
+		InitializeLine(13, 8, 10, lineWidth, Color.green);
 
-        // Left Leg
-        InitializeLine(14, 11, 13, lineWidth, Color.blue);
-        InitializeLine(15, 13, 15, lineWidth, Color.blue);
-        // Right Leg
-        InitializeLine(16, 12, 14, lineWidth, Color.blue);
-        InitializeLine(17, 14, 16, lineWidth, Color.blue);
-    }
+		// Left Leg
+		InitializeLine(14, 11, 13, lineWidth, Color.blue);
+		InitializeLine(15, 13, 15, lineWidth, Color.blue);
+		// Right Leg
+		InitializeLine(16, 12, 14, lineWidth, Color.blue);
+		InitializeLine(17, 14, 16, lineWidth, Color.blue);
+	}
 
-	private void RenderSkeleton() {
-        for (int i = 0; i < jointPairs.Length; i++)
-        {
-            int startpointIndex = jointPairs[i][0];
-            int endpointIndex = jointPairs[i][1];
+	private void RenderSkeleton()
+	{
+		for (int i = 0; i < jointPairs.Length; i++)
+		{
+			int startpointIndex = jointPairs[i][0];
+			int endpointIndex = jointPairs[i][1];
 
-            GameObject startingKeyPoint = keypoints[startpointIndex];
-            GameObject endingKeyPoint = keypoints[endpointIndex];
+			GameObject startingKeyPoint = keypoints[startpointIndex];
+			GameObject endingKeyPoint = keypoints[endpointIndex];
 
-            Vector3 startPos = new Vector3(startingKeyPoint.transform.position.x,
-                                           startingKeyPoint.transform.position.y,
-                                           startingKeyPoint.transform.position.z);
-            Vector3 endPos = new Vector3(endingKeyPoint.transform.position.x,
-                                         endingKeyPoint.transform.position.y,
-                                         endingKeyPoint.transform.position.z);
+			Vector3 startPos = new Vector3(startingKeyPoint.transform.position.x,
+										   startingKeyPoint.transform.position.y,
+										   startingKeyPoint.transform.position.z);
+			Vector3 endPos = new Vector3(endingKeyPoint.transform.position.x,
+										 endingKeyPoint.transform.position.y,
+										 endingKeyPoint.transform.position.z);
 
-            if (startingKeyPoint.activeInHierarchy && endingKeyPoint.activeInHierarchy) {
-                lineRenderers[i].gameObject.SetActive(true);
-                lineRenderers[i].SetPosition(0, startPos);
-                lineRenderers[i].SetPosition(1, endPos);
-            } else {
-                lineRenderers[i].gameObject.SetActive(false);
-            }
-        }
-    }
+			if (startingKeyPoint.activeInHierarchy && endingKeyPoint.activeInHierarchy)
+			{
+				lineRenderers[i].gameObject.SetActive(true);
+				lineRenderers[i].SetPosition(0, startPos);
+				lineRenderers[i].SetPosition(1, endPos);
+			}
+			else
+			{
+				lineRenderers[i].gameObject.SetActive(false);
+			}
+		}
+	}
 
 
 }
--- Assets/Scripts/Runtime/PoseDetection.cs
+++ Assets/Scripts/Runtime/PoseDetection.cs
@@ -4,45 +4,45 @@ using UnityEngine.Video;
 
 namespace Pose.Detection
 {
-    /// <summary>
-    /// Pose Detection using Unity Barracuda.
-    /// 1. Use an existing pose detection model (HRNet, DeepPose etc) and convert it to onnx model.
-    /// 2. Render video to a RenderTarget and use it as the input for the model.
-    /// 3. Preprocess the render target before passing it to the model. You can use Compute Shaders for this.
-    /// 4. Each model requires a specific color space and resolution, you need to convert the render target first.
-    /// 5. The `Model` object along with `IWorker` engine classes from Barracuda package will help you with inference.
-    /// 6. Convert the output key points with confidence to actual pose and joint values.
-    /// 7. Drive a skeleton/gameobjects with joint values.
-    /// </summary>
-    public class PoseDetection : MonoBehaviour
-    {
-        [SerializeField] private VideoPlayer videoPlayer;
-        [SerializeField] private ComputeShader preprocessingShader;
-        public RenderTexture videoTexture;
+	/// <summary>
+	/// Pose Detection using Unity Barracuda.
+	/// 1. Use an existing pose detection model (HRNet, DeepPose etc) and convert it to onnx model.
+	/// 2. Render video to a RenderTarget and use it as the input for the model.
+	/// 3. Preprocess the render target before passing it to the model. You can use Compute Shaders for this.
+	/// 4. Each model requires a specific color space and resolution, you need to convert the render target first.
+	/// 5. The `Model` object along with `IWorker` engine classes from Barracuda package will help you with inference.
+	/// 6. Convert the output key points with confidence to actual pose and joint values.
+	/// 7. Drive a skeleton/gameobjects with joint values.
+	/// </summary>
+	public class PoseDetection : MonoBehaviour
+	{
+		[SerializeField] private VideoPlayer videoPlayer;
+		[SerializeField] private ComputeShader preprocessingShader;
+		public RenderTexture videoTexture;
 
-        [Space]
-        [SerializeField] private int imageHeight = 352;
-        [SerializeField] private int imageWidth = 352;
+		[Space]
+		[SerializeField] private int imageHeight = 352;
+		[SerializeField] private int imageWidth = 352;
 
-        [Space]
-        [SerializeField] private GameObject videoQuad;
+		[Space]
+		[SerializeField] private GameObject videoQuad;
 
 		public bool displayInput = false;
 		public GameObject inputScreen;
-        public RenderTexture inputTexture;
+		public RenderTexture inputTexture;
 
 
-        #region private
-        private int _videoHeight;
-        private int _videoWidth;
+		#region private
+		private int _videoHeight;
+		private int _videoWidth;
 
-        private const int numKeypoints = 17;
-        // Estimated 2D keypoint locations in videoTexture and their associated confidence values
-        private float[][] keypointLocations = new float[numKeypoints][];
+		private const int numKeypoints = 17;
+		// Estimated 2D keypoint locations in videoTexture and their associated confidence values
+		private float[][] keypointLocations = new float[numKeypoints][];
 
-        //Shader Property
-        private static readonly int MainTex = Shader.PropertyToID("_MainTex");
-        #endregion
+		//Shader Property
+		private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+		#endregion
 
 		public NNModel modelAsset;
 		public WorkerFactory.Type workerType = WorkerFactory.Type.Auto;
@@ -60,48 +60,51 @@ namespace Pose.Detection
 		private float[][] prevPos = new float[numKeypoints][];
 		private float[][] prevVelocity = new float[numKeypoints][];
 
-        private void Start()
-        {
-            _videoHeight = (int)videoPlayer.GetComponent<VideoPlayer>().height;
-            _videoWidth = (int)videoPlayer.GetComponent<VideoPlayer>().width;
+		private void Start()
+		{
+			_videoHeight = (int)videoPlayer.GetComponent<VideoPlayer>().height;
+			_videoWidth = (int)videoPlayer.GetComponent<VideoPlayer>().width;
 
-            // Create a new videoTexture using the current video dimensions
-            videoTexture = new RenderTexture(_videoWidth, _videoHeight, 24, RenderTextureFormat.ARGB32);
-            videoPlayer.GetComponent<VideoPlayer>().targetTexture = videoTexture;
+			// Create a new videoTexture using the current video dimensions
+			videoTexture = new RenderTexture(_videoWidth, _videoHeight, 24, RenderTextureFormat.ARGB32);
+			videoPlayer.GetComponent<VideoPlayer>().targetTexture = videoTexture;
 
-            //Apply Texture to Quad
-            videoQuad.gameObject.GetComponent<MeshRenderer>().material.SetTexture(MainTex, videoTexture);
-            videoQuad.transform.localScale = new Vector3(_videoWidth, _videoHeight, videoQuad.transform.localScale.z);
-            videoQuad.transform.position = new Vector3(_videoWidth / 2, _videoHeight / 2, 1);
+			//Apply Texture to Quad
+			videoQuad.gameObject.GetComponent<MeshRenderer>().material.SetTexture(MainTex, videoTexture);
+			videoQuad.transform.localScale = new Vector3(_videoWidth, _videoHeight, videoQuad.transform.localScale.z);
+			videoQuad.transform.position = new Vector3(_videoWidth / 2, _videoHeight / 2, 1);
 
-            //Move Camera to keep Quad in view
-            var mainCamera = Camera.main;
-            if (mainCamera !=null)
-            {
-                mainCamera.transform.position = new Vector3(_videoWidth / 2, _videoHeight / 2, -(_videoWidth / 2));
-                mainCamera.GetComponent<Camera>().orthographicSize = _videoHeight / 2;
-            }
+			//Move Camera to keep Quad in view
+			var mainCamera = Camera.main;
+			if (mainCamera != null)
+			{
+				mainCamera.transform.position = new Vector3(_videoWidth / 2, _videoHeight / 2, -(_videoWidth / 2));
+				mainCamera.GetComponent<Camera>().orthographicSize = _videoHeight / 2;
+			}
 
 			m_RuntimeModel = ModelLoader.Load(modelAsset);
 
 			var modelBuilder = new ModelBuilder(m_RuntimeModel);
 
 			engine = WorkerFactory.CreateWorker(workerType, modelBuilder.model);
-        }
+		}
 
 
-        // Unity method that is called every tick/frame
-        private void Update()
-        {
-            Texture2D processedImage = PreprocessTexture();
+		// Unity method that is called every tick/frame
+		private void Update()
+		{
+			Texture2D processedImage = PreprocessTexture();
 
-			if (displayInput) {
+			if (displayInput)
+			{
 				inputScreen.SetActive(true);
 				// Graphics.Blit(processedImage, inputTexture);
 				Texture2D scaledInputImage = ScaleInputImage(processedImage);
 				Graphics.Blit(scaledInputImage, inputTexture);
 				Destroy(scaledInputImage);
-			} else {
+			}
+			else
+			{
 				inputScreen.SetActive(false);
 			}
 
@@ -116,117 +119,119 @@ namespace Pose.Detection
 
 			input.Dispose();
 
-            Destroy(processedImage);
-        }
+			Destroy(processedImage);
+		}
 
-        private void OnDisable()
-        {
+		private void OnDisable()
+		{
 			engine.Dispose();
 
-            //Release videoTexture
-            videoTexture.Release();
-        }
+			//Release videoTexture
+			videoTexture.Release();
+		}
 
 
-        #region Additional Methods
+		#region Additional Methods
 
-        private Texture2D PreprocessTexture()
-        {
-            //Apply any kind of preprocessing if required - Resize, Color values scaled etc
+		private Texture2D PreprocessTexture()
+		{
+			//Apply any kind of preprocessing if required - Resize, Color values scaled etc
 
-            Texture2D imageTexture = new Texture2D(videoTexture.width,
-                videoTexture.height, TextureFormat.RGBA32, false);
+			Texture2D imageTexture = new Texture2D(videoTexture.width,
+				videoTexture.height, TextureFormat.RGBA32, false);
 
-            Graphics.CopyTexture(videoTexture, imageTexture);
-            Texture2D tempTex = Resize(imageTexture, imageHeight, imageWidth);
-            Destroy(imageTexture);
+			Graphics.CopyTexture(videoTexture, imageTexture);
+			Texture2D tempTex = Resize(imageTexture, imageHeight, imageWidth);
+			Destroy(imageTexture);
 
-            imageTexture = PreprocessNetwork(tempTex);
+			imageTexture = PreprocessNetwork(tempTex);
 
-            Destroy(tempTex);
-            return imageTexture;
-        }
+			Destroy(tempTex);
+			return imageTexture;
+		}
 
-        private Texture2D Resize(Texture2D image, int newWidth, int newHeight)
-        {
-            RenderTexture rTex = RenderTexture.GetTemporary(newWidth, newHeight, 24);
-            RenderTexture.active = rTex;
+		private Texture2D Resize(Texture2D image, int newWidth, int newHeight)
+		{
+			RenderTexture rTex = RenderTexture.GetTemporary(newWidth, newHeight, 24);
+			RenderTexture.active = rTex;
 
-            Graphics.Blit(image, rTex);
-            Texture2D nTex = new Texture2D(newWidth, newHeight, TextureFormat.RGBA32, false);
+			Graphics.Blit(image, rTex);
+			Texture2D nTex = new Texture2D(newWidth, newHeight, TextureFormat.RGBA32, false);
 
-            Graphics.CopyTexture(rTex, nTex);
-            RenderTexture.active = null;
+			Graphics.CopyTexture(rTex, nTex);
+			RenderTexture.active = null;
 
-            RenderTexture.ReleaseTemporary(rTex);
-            return nTex;
-        }
+			RenderTexture.ReleaseTemporary(rTex);
+			return nTex;
+		}
 
-        private Texture2D PreprocessNetwork(Texture2D inputImage)
-        {
-            // Use Compute Shaders (GPU) to preprocess your image
-            // Each model requires a specific color space - RGB
-            // Values need to scaled to what it was trained on
+		private Texture2D PreprocessNetwork(Texture2D inputImage)
+		{
+			// Use Compute Shaders (GPU) to preprocess your image
+			// Each model requires a specific color space - RGB
+			// Values need to scaled to what it was trained on
 
-            var numthreads = 8;
-            var kernelHandle = preprocessingShader.FindKernel("Standardize");
-            var rTex = new RenderTexture(inputImage.width,
-                inputImage.height, 24, RenderTextureFormat.ARGBHalf);
-            rTex.enableRandomWrite = true;
-            rTex.Create();
+			var numthreads = 8;
+			var kernelHandle = preprocessingShader.FindKernel("Standardize");
+			var rTex = new RenderTexture(inputImage.width,
+				inputImage.height, 24, RenderTextureFormat.ARGBHalf);
+			rTex.enableRandomWrite = true;
+			rTex.Create();
 
-            preprocessingShader.SetTexture(kernelHandle, "Result", rTex);
-            preprocessingShader.SetTexture(kernelHandle, "InputImage", inputImage);
-            preprocessingShader.Dispatch(kernelHandle, inputImage.height
-                                                       / numthreads,
-                inputImage.width / numthreads, 1);
+			preprocessingShader.SetTexture(kernelHandle, "Result", rTex);
+			preprocessingShader.SetTexture(kernelHandle, "InputImage", inputImage);
+			preprocessingShader.Dispatch(kernelHandle, inputImage.height
+													   / numthreads,
+				inputImage.width / numthreads, 1);
 
-            RenderTexture.active = rTex;
-            Texture2D nTex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGBAHalf, false);
-            Graphics.CopyTexture(rTex, nTex);
-            RenderTexture.active = null;
+			RenderTexture.active = rTex;
+			Texture2D nTex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGBAHalf, false);
+			Graphics.CopyTexture(rTex, nTex);
+			RenderTexture.active = null;
 
-            Destroy(rTex);
-            return nTex;
-        }
+			Destroy(rTex);
+			return nTex;
+		}
 
-		private void ProcessResults(Tensor heatmaps) {
+		private void ProcessResults(Tensor heatmaps)
+		{
 			float stride = imageHeight / heatmaps.shape.height;
 
-			var scaleY = (float) videoTexture.height / (float) imageHeight;
-			var scaleX = (float) videoTexture.width / (float) imageWidth;
+			var scaleY = (float)videoTexture.height / (float)imageHeight;
+			var scaleX = (float)videoTexture.width / (float)imageWidth;
 
-			for (int k = 0; k < numKeypoints; k++) {
+			for (int k = 0; k < numKeypoints; k++)
+			{
 				var locationInfo = LocateKeyPoint(heatmaps, k);
-                var coords = locationInfo.Item1;
-                var confidenceValue = locationInfo.Item2;
+				var coords = locationInfo.Item1;
+				var confidenceValue = locationInfo.Item2;
 
-                var xPos = coords[0]*stride*scaleX;
-                var yPos = (imageHeight - (coords[1]*stride))*scaleY;
+				var xPos = coords[0] * stride * scaleX;
+				var yPos = (imageHeight - (coords[1] * stride)) * scaleY;
 
-                keypointLocations[k] = new float[] { xPos, yPos, confidenceValue };
+				keypointLocations[k] = new float[] { xPos, yPos, confidenceValue };
 			}
 		}
 
-        private (float[],float) LocateKeyPoint(Tensor heatmaps, int i)
-        {
-            //Find the heatmap index that contains the highest confidence value and the associated offset vector
-            var maxConfidence = 0f;
-            var coords = new float[2];
+		private (float[], float) LocateKeyPoint(Tensor heatmaps, int i)
+		{
+			//Find the heatmap index that contains the highest confidence value and the associated offset vector
+			var maxConfidence = 0f;
+			var coords = new float[2];
 
-            for (int y = 0; y < heatmaps.shape.height; y++)
-            {
-                for (int x = 0; x < heatmaps.shape.width; x++)
-                {
-                    if (heatmaps[0, y, x, i] > maxConfidence)
-                    {
-                        maxConfidence = heatmaps[0, y, x, i];
-                        coords = new float[] { x, y };
-                    }
-                }
-            }
-            return (coords, maxConfidence);
-        }
+			for (int y = 0; y < heatmaps.shape.height; y++)
+			{
+				for (int x = 0; x < heatmaps.shape.width; x++)
+				{
+					if (heatmaps[0, y, x, i] > maxConfidence)
+					{
+						maxConfidence = heatmaps[0, y, x, i];
+						coords = new float[] { x, y };
+					}
+				}
+			}
+			return (coords, maxConfidence);
+		}
 
 		private Texture2D ScaleInputImage(Texture2D inputImage)
 		{
@@ -263,12 +268,17 @@ namespace Pose.Detection
 			return nTex;
 		}
 
-		private void UpdateKeyPointPositions() {
-			for (int k = 0; k < numKeypoints; k++) {
-				if (keypointLocations[k][2] >= minConfidence / 100f) {
+		private void UpdateKeyPointPositions()
+		{
+			for (int k = 0; k < numKeypoints; k++)
+			{
+				if (keypointLocations[k][2] >= minConfidence / 100f)
+				{
 					keypoints[k].SetActive(true);
 					//keypoints[k].activeInHierarchy
-				} else {
+				}
+				else
+				{
 					keypoints[k].SetActive(false);
 				}
 
@@ -279,15 +289,22 @@ namespace Pose.Detection
 
 		// Simple extrapolation of keypoints when the current frame's joint keypoints
 		// is not available. If not available: use prevPos + prevVelocity position.
-		private void FillAndUpdatePrevPos() {
-			for (int k = 0; k < numKeypoints; k++) {
-				if (keypointLocations[k][2] < minConfidence / 100f) {
+		private void FillAndUpdatePrevPos()
+		{
+			for (int k = 0; k < numKeypoints; k++)
+			{
+				if (keypointLocations[k][2] < minConfidence / 100f)
+				{
 					// Fill in the keypoints if possible
-					if (prevPos[k] != null && prevPos[k][2] >= minConfidence / 100f) {
+					if (prevPos[k] != null && prevPos[k][2] >= minConfidence / 100f)
+					{
 						Vector3 newPos;
-						if (prevVelocity[k] != null) {
+						if (prevVelocity[k] != null)
+						{
 							newPos = new Vector3(prevPos[k][0], prevPos[k][1], -1f);
-						} else {
+						}
+						else
+						{
 							newPos = new Vector3(prevPos[k][0] + prevVelocity[k][0],
 								prevPos[k][1] + prevVelocity[k][1],
 								-1f);
@@ -295,22 +312,27 @@ namespace Pose.Detection
 						keypoints[k].transform.position = newPos;
 						keypoints[k].SetActive(true);
 					}
-					prevPos[k] = new float[] { 0, 0, -1};
-					prevVelocity[k] = new float[] {0, 0, -1};
-				} else {  // update the prev positions and velocities
-					if (prevPos[k] != null) {
+					prevPos[k] = new float[] { 0, 0, -1 };
+					prevVelocity[k] = new float[] { 0, 0, -1 };
+				}
+				else
+				{  // update the prev positions and velocities
+					if (prevPos[k] != null)
+					{
 						prevVelocity[k] = new float[] {
 							keypointLocations[k][0] - prevPos[k][0],
 							keypointLocations[k][1] - prevPos[k][1],
 							1 };
-					} else {
-						prevVelocity[k] = new float[] {0, 0, -1};
+					}
+					else
+					{
+						prevVelocity[k] = new float[] { 0, 0, -1 };
 					}
 					prevPos[k] = keypointLocations[k];
 				}
 			}
 		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
